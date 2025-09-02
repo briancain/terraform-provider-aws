8fe895c22a9fda53f8f4536e53a8947534c66155
Brian Cain <briancq@amazon.com>
2025-09-02 13:13:10 -0700

    Add tests

--- internal/service/arcregionswitch/plan_complex_test.go
+++ internal/service/arcregionswitch/plan_complex_test.go
@@ -586,7 +586,7 @@ resource "aws_vpc" "primary" {
 }
 
 resource "aws_vpc" "secondary" {
-  provider = awsalternate
+  provider             = awsalternate
   cidr_block           = "10.2.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
